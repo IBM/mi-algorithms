858ebe7ce76abb61d9b94eca39e1bb00858141bf
Tomasz Kornuta <tkornut@us.ibm.com>
2016-02-17 18:44:23 -0800

    Class MatrixXd with OpenBLAS boosting! + matrix test application

--- src/tests/matrix_test.cpp
+++ src/tests/matrix_test.cpp
@@ -5,14 +5,23 @@
  * \date Feb 17, 2016
  */
 
-#include <types/MatrixTypes.hpp>
+
 #include <iostream>
-#include <random>
+#include <iomanip>
+
 #include <sys/time.h>
 
-#ifdef OpenBLAS_FOUND
-#include <cblas.h>
-#endif
+#include <types/MatrixTypes.hpp>
+#include <data_utils/functions.h>
+
+
+namespace mic {
+namespace types {
+
+
+}//: namespace types
+}//: namespace mic
+
 
 class Timer {
 
@@ -52,7 +61,10 @@ class Timer {
 #ifdef OpenBLAS_FOUND
 // c = a * b
 void BLAS_mmul( Eigen::MatrixXf& __restrict c, Eigen::MatrixXf& __restrict a,
-		Eigen::MatrixXf& __restrict b, bool aT = false, bool bT = false ) {
+		Eigen::MatrixXf& __restrict b, bool aT = false, bool bT = false) {
+
+	// Both matrices are not transposed - as default.
+	// Transposition is a different class in EIGEN.
 
 	enum CBLAS_TRANSPOSE transA = aT ? CblasTrans : CblasNoTrans;
 	enum CBLAS_TRANSPOSE transB = bT ? CblasTrans : CblasNoTrans;
@@ -62,7 +74,7 @@ void BLAS_mmul( Eigen::MatrixXf& __restrict c, Eigen::MatrixXf& __restrict a,
 	size_t K = aT ? a.rows() : a.cols();
 
 	float alpha = 1.0f;
-	float beta = 1.0f;
+	float beta = 0.0f;
 
 	size_t lda = aT ? K : M;
 	size_t ldb = bT ? N : K;
@@ -74,26 +86,31 @@ void BLAS_mmul( Eigen::MatrixXf& __restrict c, Eigen::MatrixXf& __restrict a,
 
 
 }
+
+// returns a * b
+Eigen::MatrixXf BLAS_mmultilpy(Eigen::MatrixXf& __restrict a, Eigen::MatrixXf& __restrict b) {
+
+	// Both matrices are not transposed - as default.
+	// Transposition is a different class in EIGEN.
+
+	size_t M = a.rows();
+	size_t K =  a.cols();
+	size_t N = b.cols();
+
+	Eigen::MatrixXf c(M,N);
+
+	cblas_sgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0,
+				 a.data(), M,
+				 b.data(), K, 0.0, c.data(), M );
+
+//	std::cout <<"c=\n" << c <<  std::endl;
+
+	return c;
+}
+
 #endif /* USE_BLAS */
 
 
-void randn(Eigen::MatrixXf& m, float mean, float stddev) {
-
-	// random number generator
-	// unfortunately, Eigen does not implement normal distribution
-	// TODO: make it cleaner, more parallel
-
-	std::random_device rd;
-	std::mt19937 mt(rd());
-	std::normal_distribution<> randn(mean, stddev);
-
-	for (int i = 0; i < m.rows(); i++) {
-		for (int j = 0; j < m.cols(); j++) {
-			m.coeffRef(i, j) = randn(mt);
-		}
-	}
-
-}
 
 
 /*!
@@ -112,34 +129,60 @@ int main(int argc, char* argv[]) {
 #endif
 
 	// Default sizes of matrices.
-	const size_t N = 64;
-	const size_t M = 256;
-	const size_t K = 128;
+	const size_t N = 200;
+	const size_t M = 300;
+	const size_t K = 400;
 	const size_t number_of_multiplications = 100;
 
 	// Matrices.
-	Eigen::MatrixXf nm(N, M);
-	Eigen::MatrixXf mk(M, K);
-	Eigen::MatrixXf nk(N, K);
+	mic::types::MatrixXf nm(N, M);
+	mic::types::MatrixXf mk(M, K);
+	mic::types::MatrixXf nk(N, K);
 
 	// Initialize matrices with random numbers.
-	randn(nm, 1, 0.001);
-	randn(mk, 0, 1.0);
+	nm.normRandReal(1, 0.001);
+	mk.normRandReal(1, 0.001);
+	std::cout <<"nm=\n" << nm <<  std::endl;
+
+	// Adding test.
+	Eigen::VectorXf vect(N);
+	vect.setRandom();
+	std::cout <<"vect=\n" << vect <<  std::endl;
+	nm.colwise() += vect;
+	std::cout <<"nm+=vect\n" << nm <<  std::endl;
+
+	// Elementwise function test.
+	mic::types::MatrixXf nm2(N, M);
+	nm2.normRandReal(1, 0.00001);
+	std::cout <<"nm2=\n" << nm2 <<  std::endl;
+	nm.elementwiseFunctionMatrix(&_add, nm2);
+	std::cout <<"nm+=nm2\n" << nm <<  std::endl;
+
+
+	std::cout <<"mk=\n" << mk <<  std::endl;
 
 	Timer timer;
 	timer.start();
 
 	for (size_t i = 0; i < number_of_multiplications; i++) {
-	// Multiplication.
-#ifdef OpenBLAS_FOUND
+		//std::cout <<  std::setfill('=') << std::setw(80)<< "=\n" <<std::setfill(' ');
+
+		// Multiplication.
+		nk = nm * mk;
+
+/*#ifdef OpenBLAS_FOUND
 		BLAS_mmul(nk, nm, mk);
+		//nk = BLAS_mmultilpy(nm, mk);
+		//std::cout <<"blas nk=\n" << nk <<  std::endl;
 #else
 		nk = nm * mk;
-#endif
+		//std::cout <<"eigen nk=\n" << nk <<  std::endl;
+#endif*/
 	}//: for
-
 	double time = timer.end();
-	std::cout  <<  "time = " << time <<  std::endl;
+	std::cout <<"nk=\n" << nk <<  std::endl;
+
+	std::cout  <<  "Multiplication time = " << time <<  std::endl;
 
 }//: main
 
--- src/types/MatrixTypes.hpp
+++ src/types/MatrixTypes.hpp
@@ -10,6 +10,12 @@
 
 #include <Eigen/Dense>
 
+#ifdef OpenBLAS_FOUND
+#include <cblas.h>
+#endif
+
+#include <random>
+
 namespace mic {
 namespace types {
 
@@ -72,6 +78,128 @@ typedef std::pair< matrixd_ptr_t, std::shared_ptr<unsigned int> > matrixd_uint_p
  * \author tkornuta
  */
 typedef std::pair< std::shared_ptr<char>, std::shared_ptr<char> > char_char_pair_t;
+
+
+
+
+/*!
+ * \brief Matrix with single precision floats (of dynamic size).
+ * Uses OpenBLAS if found by CMAKE.
+ *
+ * \date Feb 17, 2016
+ * \author tkornuta
+ */
+class MatrixXf : public Eigen::MatrixXf {
+public:
+	/*!
+	 * Constructor. Calls default Eigen::MatrixXf constructor.
+	 * @param Rows_ Number of rows.
+	 * @param Cols_ Number of columns.
+	 */
+	MatrixXf (int Rows_, int Cols_) : Eigen::MatrixXf(Rows_, Cols_) { };
+
+	/*!
+	 * Overloaded operator - calls base operator.
+	 */
+    EIGEN_STRONG_INLINE Eigen::MatrixXf& operator =(const Eigen::MatrixXf& mat_)
+    {
+    	// Using base EIGEN operator =
+    	return Base::operator=(mat_); // work
+	}
+
+
+    /*!
+     * Overloaded matrix multiplication operator. Uses OpenBLAS if found by CMAKE.
+     */
+    EIGEN_STRONG_INLINE Eigen::MatrixXf operator *(const Eigen::MatrixXf& mat_)
+    {
+
+    	#ifdef OpenBLAS_FOUND
+    	// Using openBLAS boost!
+			// Get dimensions.
+			size_t M = this->rows();
+			size_t K =  this->cols();
+			size_t N = mat_.cols();
+			// Create temporary matrix.
+			Eigen::MatrixXf c(M,N);
+
+			cblas_sgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0,
+					this->data(), M,
+					mat_.data(), K, 0.0, c.data(), M );
+			return c;
+		#else
+			// Calling base EIGEN operator *
+			return Base::operator*(mat_);
+		#endif
+    }
+
+    /*!
+     * Set values of all matrix elements to random with a normal distribution.
+     * @param mean Mean
+     * @param stddev Variance
+     */
+    void normRandReal(float mean, float stddev) {
+
+    	// Initialize random number generator with normal distribution.
+    	std::random_device rd;
+    	std::mt19937 mt(rd());
+    	std::normal_distribution<> randn(mean, stddev);
+
+    	// Get access to data.
+    	float* data_ptr = this->data();
+
+		#pragma omp parallel for
+    	for (int i = 0; i < this->rows()*this->cols(); i++) {
+    		data_ptr[i] = randn(mt);
+		}
+	}
+
+
+	void elementwiseFunctionMatrix(float (*func)(float, float), Eigen::MatrixXf & mat_) {
+
+		// Check dimensions.
+		if ((this->rows() != mat_.rows()) || (this->cols() != mat_.cols()))
+			printf("elementwise_function_matrix, dim mismatch!\n");
+
+    	// Get access to data.
+    	float* data_ptr = this->data();
+    	float* m_data_ptr = mat_.data();
+
+    	// Apply function to all elements.
+		#pragma omp parallel for
+		for (size_t i = 0; i < rows()*cols(); i++) {
+			data_ptr[i] = (*func)(data_ptr[i], m_data_ptr[i]);
+		}
+
+	}
+
+};
+
+
+/*!
+ * \brief Shared pointer to matrix with single precision floats (of dynamic size).
+ * \author tkornuta
+ */
+typedef std::shared_ptr< mic::types::MatrixXf > matrixf_ptr_t;
+
+
+/*!
+ * \brief The <single precision float matrix-char> pair type.
+ * \author tkornuta
+ */
+typedef std::pair< matrixf_ptr_t, std::shared_ptr<char> > matrixf_char_pair_t;
+
+/*!
+ * \brief The <single precision float matrix-unsigned int> pair.
+ * \author tkornuta
+ */
+typedef std::pair< matrixf_ptr_t, std::shared_ptr<unsigned int> > matrixf_uint_pair_t;
+
+
+
+
+
+
 
 
 }//: namespace types
