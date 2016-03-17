aa2fbda1fe62ecfea35d891f48037da09cd39d90
Tomasz Kornuta <tkornut@us.ibm.com>
2016-03-17 10:44:36 -0700

    Added header with several predefined tensor typedefs, analogical to MatrixX(i/f/d)

---
+++ src/types/TensorTypes.hpp
@@ -0,0 +1,56 @@
+/*!
+ * \file TensorTypes.hpp
+ * \brief Contains declaration of tensor types.
+ * \author tkornut
+ * \date Mar 17, 2016
+ */
+
+#ifndef SRC_TYPES_TENSORTYPES_HPP_
+#define SRC_TYPES_TENSORTYPES_HPP_
+
+#include <types/Tensor.hpp>
+
+/*!
+ * \brief Tensor of integers (of dynamic size).
+ * \author tkornuta
+ */
+typedef mic::types::Tensor<int> TensorXi;
+
+
+/*!
+ * \brief Shared pointer to tensor of integers (of dynamic size).
+ * \author tkornuta
+ */
+typedef std::shared_ptr< mic::types::TensorXi > TensorXiPtr;
+
+
+/*!
+ * \brief Tensor of single precision floats (of dynamic size).
+ * \author tkornuta
+ */
+typedef mic::types::Tensor<double> TensorXf;
+
+
+/*!
+ * \brief Shared pointer to tensor of single precision floats (of dynamic size).
+ * \author tkornuta
+ */
+typedef std::shared_ptr< mic::types::TensorXf > TensorXfPtr;
+
+
+/*!
+ * \brief Tensor of double precision floats (of dynamic size).
+ * \author tkornuta
+ */
+typedef mic::types::Tensor<double> TensorXd;
+
+
+/*!
+ * \brief Shared pointer to tensor of double precision floats (of dynamic size).
+ * \author tkornuta
+ */
+typedef std::shared_ptr< mic::types::TensorXd > TensorXdPtr;
+
+
+
+#endif /* SRC_TYPES_TENSORTYPES_HPP_ */
